9d65f9a76db0f69208c13c34f1a4030aca65052f
nirjhar sarkar <nirjharsarkar8@gmail.com>
2025-02-04 11:05:33 +0530

    fixed the bugs and UI errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
-import "./globals.css"
+import "~/styles/globals.css";
 import { Inter } from "next/font/google"
-import { Providers } from "./providers"
+import { Providers } from "~/lib/provider"
 import type React from "react" // Added import for React
 
 const inter = Inter({ subsets: ["latin"] })
